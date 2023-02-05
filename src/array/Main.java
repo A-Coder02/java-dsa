0726817147dd0b2fd4a0d42fea30a1818a0adf16
Arbaj Ansari <arbaj897ansari@gmail.com>
2023-02-05 12:34:11 +0530

    array basic completed

---
+++ src/array/Main.java
@@ -0,0 +1,18 @@
+package array;
+
+public class Main {
+    public static void main(String[] args) {
+        // syntax
+        // datatype[] variable_name = new datatype[size];
+        // store 5 roll numbers:
+        int[] rnos = new int[5];
+        int[] rnos2 = {23,43,65,85,244};
+
+        int[] ros;
+
+        String[] arr = new String[4];
+
+        System.out.println(arr[0]);
+
+    }
+}
